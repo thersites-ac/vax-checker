bfd154ad3e408650318baf940247756235262cb5
Aaron Cogbill <aaaronc@gmail.com>
2021-03-14 18:25:45 -0400

    remove bootstrap.sh

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-# almost the entire bootstrap sequence -- does not set environment variables
-sudo su
-yum update
-yum install -y git
-yum install -y docker
-git clone https://www.github.com/thersites-ac/vax-checker
-cd vax-checker
-./launch CVS
-./launch RiteAid
